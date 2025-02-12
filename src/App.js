2942addfb504ed2b72ef4c1dba45c2d8d083e2fe
Иван Лушников <vanyalush2000@gmail.com>
2025-02-12 14:16:48 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {useEffect} from "react";
+function App() {
+
+    useEffect(() => {
+        tg.ready();
+    }, )
+  return (
+    <div className="App">
+      work
+    </div>
+  );
+}
+
+export default App;
